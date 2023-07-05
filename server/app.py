a53ed34b9ce80a2169c76fa5fbd56372935d0354
Dovid Yarmush <yarmushda@gmail.com>
2023-07-05 15:38:49 -0400

    api crud lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,57 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET', 'POST'])
+def get_messages():
 
-@app.route('/messages/<int:id>')
+    messages = []
+    
+    if request.method == 'GET':
+        for message in Message.query.all():
+            messages.append(message.to_dict())
+        return make_response(messages, 200)
+
+    elif request.method == 'POST':
+        new_message_dict = request.get_json()
+        new_message = Message(
+            body=new_message_dict['body'],
+            username=new_message_dict['username'],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(new_message_dict, 201) 
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+
+    if message == None:
+        response = {
+            'message': 'No matching message in the database'
+        }
+        return make_response(response, 404)
+    else:
+        if request.method == 'PATCH':
+            json_message = request.get_json()
+            for attr in json_message:
+                setattr(message, attr, json_message.get(attr))
+
+                db.session.add(message)
+                db.session.commit()
+
+            return make_response(message.to_dict(), 200)
+        
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            return make_response(
+                jsonify({'status': 'delete successful'}),
+                200
+            )
+
 
 if __name__ == '__main__':
     app.run(port=5555)
